652dfe55c5bb8643556aa84df3e33b673e07dc5d
nightcrawler0112 <rajnavinraj23@gmail.com>
2023-05-11 16:13:40 +0530

    seek baar updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Welcome to Musify");
 
 //variables
 let songindex=0;
-let audioElement = new Audio('1.mp3');
+let audioElement = new Audio('playlist/1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myprogressbar');
 
@@ -10,12 +10,12 @@ let myProgressBar=document.getElementById('myprogressbar');
 
 
 let songs = [
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"},
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"},
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"},
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"},
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"},
-    {songname: "name of the song", filepath: "pathofthesong", coverpath:"covers/1.jpg"}
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
+    {songname: "name of the song", filepath: "playlist/1.mp3", coverpath:"images\song1.jpg"},
 ]
 
 
@@ -24,16 +24,32 @@ let songs = [
 //pause/play
 masterPlay.addEventListener('click',()=>{
    if(audioElement.paused || audioElement.currentTime<=0){
-    audioElement.play;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+   }
+
+   else{
+    audioElement.pause();
+    masterPlay.classList.remove('fa-pause-circle');
+    masterPlay.classList.add('fa-play-circle');
+
    }
 }
 )
 //Listen to Events
-myProgressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
 
     console.log('timeupdate');
     //update seekbar
+    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+    //console.log(progress);
+    myProgressBar.value=progress;
 
-}
-)
+})
+
+myProgressBar.addEventListener('change',()=>{
+    audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
+})
 
